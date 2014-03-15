53248d451113bfefe47a26752aee00980a8c1e74
James Myers <jfmyers9@gmail.com>
2014-03-15 14:26:24 -0700

    You can now specify the largest starting number for problem 14

--- p14/LongestCollatzSequence.java
+++ p14/LongestCollatzSequence.java
@@ -4,11 +4,17 @@ public class LongestCollatzSequence {
 
   public static int[] lengths;
 
-  public static void main(String[] argv) {
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("You must pass in the max number that we can start from.");
+    }
+
+    int n = Integer.parseInt(args[0]);
+
     int max_length = 0;
     int max_index = 0;
 
-    for (int i = 1; i < 1000000; i++) {
+    for (int i = 1; i < n; i++) {
       int length = computeCollatzLength(i);
       if (length > max_length) {
         max_length = length;
