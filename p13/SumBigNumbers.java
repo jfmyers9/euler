f981f76e8e94beab0164fb6780a6fabaeb9a0463
James Myers <jfmyers9@gmail.com>
2014-03-15 14:23:03 -0700

    Allow the user to specify the file of big numbers in problem 13.

--- p13/SumBigNumbers.java
+++ p13/SumBigNumbers.java
@@ -4,14 +4,18 @@ import java.io.BufferedReader;
 
 public class SumBigNumbers {
 
-  public static void main(String[] argv) {
-    sumNums();
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("You must pass in the data file of numbers.");
+    }
+
+    sumNums(args[0]);
   }
 
-  public static void sumNums() {
+  public static void sumNums(String filePath) {
     BigInteger sum = new BigInteger("0");
     try {
-      BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+      BufferedReader br = new BufferedReader(new FileReader(filePath));
       String curNum = br.readLine();
       while (curNum != null) {
         BigInteger current = new BigInteger(curNum.trim());
